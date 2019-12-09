4f30b2fb6ce5d3c26de7b22b1c95ed70c6db4dc6
Lauren Hetherington <laurenlhetherington@gmail.com>
2019-12-09 17:47:10 -0500

    Updates

---
+++ precache-manifest.b64c5fe1028796f797f07d71de58ef1f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c643360c76872895c7dcf5e7094abe23",
+    "url": "/projectSix/index.html"
+  },
+  {
+    "revision": "e70234bd771b75ee5ca6",
+    "url": "/projectSix/static/css/main.936fcc4f.chunk.css"
+  },
+  {
+    "revision": "e173564af085220f7049",
+    "url": "/projectSix/static/js/2.0645c9f4.chunk.js"
+  },
+  {
+    "revision": "e70234bd771b75ee5ca6",
+    "url": "/projectSix/static/js/main.b78af5c7.chunk.js"
+  },
+  {
+    "revision": "be28398d709b4aad0631",
+    "url": "/projectSix/static/js/runtime-main.75aeb787.js"
+  },
+  {
+    "revision": "132eafededae384de3a2cf41611ff678",
+    "url": "/projectSix/static/media/heroPlaceholder.132eafed.jpg"
+  },
+  {
+    "revision": "5ee2ff7eaf4b48a5564cc3e4bf33a8e7",
+    "url": "/projectSix/static/media/imagePlaceholder.5ee2ff7e.jpg"
+  },
+  {
+    "revision": "d034a805f18e19e07a762167addcbb87",
+    "url": "/projectSix/static/media/restaurantRecommendation1Placeholder.d034a805.jpg"
+  },
+  {
+    "revision": "52ff61e3acd11896ed7ba9e5035ee37b",
+    "url": "/projectSix/static/media/restaurantRecommendation2Placeholder.52ff61e3.jpg"
+  },
+  {
+    "revision": "f66b160a348bdf9516da43f54b24ca5a",
+    "url": "/projectSix/static/media/restaurantRecommendation3Placeholder.f66b160a.jpg"
+  },
+  {
+    "revision": "0653b58a593778df0d59f33cdb078fcf",
+    "url": "/projectSix/static/media/yelpLogo.0653b58a.png"
+  },
+  {
+    "revision": "adbe3936a57fa454811b989e9b0cd3b6",
+    "url": "/projectSix/static/media/yelpLogoIconOnly.adbe3936.png"
+  }
+]);
